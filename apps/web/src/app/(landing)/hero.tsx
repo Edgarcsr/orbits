585b426dfa817903024cdb9afbf45d2af14707f0
Edgarcsr <edgarseabra@outlook.com.br>
2026-04-01 21:02:53 -0300

    feat: add early development label to Hero component

--- apps/web/src/app/(landing)/hero.tsx
+++ apps/web/src/app/(landing)/hero.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { BoxIcon, FlaskConicalIcon, Pyramid } from 'lucide-react'
 import { motion } from 'motion/react'
 import { Button } from '@/components/ui/button'
 import { FlipWords } from '@/components/ui/flip-words'
@@ -19,6 +20,15 @@ export default function Hero() {
       className="text-4xl h-96"
     >
       <div className="h-[40rem] flex flex-col text-left justify-center px-4">
+        <motion.span
+          initial={{ opacity: 0, x: -15 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1.4 }}
+          className="backdrop-blur-2xl text-sm flex items-center gap-2 text-muted-foreground font-medium mb-4 rounded-xl w-fit px-3 py-1  border-muted bg-white/5"
+        >
+          <BoxIcon size={16} />
+          Early Development
+        </motion.span>
         <h1 className="text-4xl font-normal text-muted-foreground">
           Make your projects
           <FlipWords words={flipWords} /> <br />
